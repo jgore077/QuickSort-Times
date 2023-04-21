bc3723d1c8250db225b19ed7d953d2d29de60505
anu_sha <126127739+anu-sha19@users.noreply.github.com>
2023-04-21 16:20:16 -0400

    Update activity23.java

--- activity23.java
+++ activity23.java
@@ -1,43 +1,76 @@
+package pNew;
 import java.util.Random;
+//Anusha Ghimire and Jimmy Gore 
 public class activity23 {
-
+    // Quicksort algorithm
     public static void quickSort(int arr[], int begin, int end) {
         if (begin < end) {
+            // Partition the array and get the partition index
             int partitionIndex = partition(arr, begin, end);
     
+            // Recursively apply Quicksort to the left and right partitions
             quickSort(arr, begin, partitionIndex-1);
             quickSort(arr, partitionIndex+1, end);
         }
     }
 
+    // Partition method that selects the middle element as the pivot (x[mid] pivot policy)
     private static int partition(int arr[], int begin, int end) {
-        int pivot = arr[end];
-        int i = (begin-1);
+        // Select the middle element as the pivot
+        int mid = (begin + end) / 2;
+        int pivot = arr[mid];
+        int i = begin - 1;
     
+        // Partition the array around the pivot
         for (int j = begin; j < end; j++) {
             if (arr[j] <= pivot) {
                 i++;
-    
-                int swapTemp = arr[i];
+                int temp = arr[i];
                 arr[i] = arr[j];
-                arr[j] = swapTemp;
+                arr[j] = temp;
             }
         }
     
-        int swapTemp = arr[i+1];
-        arr[i+1] = arr[end];
-        arr[end] = swapTemp;
+        // Move the pivot to its final position
+        int temp = arr[i+1];
+        arr[i+1] = arr[mid];
+        arr[mid] = temp;
     
         return i+1;
     }
 
-    public static void main(String[] args){
-       Random rand= new Random();
-       int[] millionArray = new int[1000000];
-       for(int i=0;i<millionArray.length;i++){
-        millionArray[i]= rand.nextInt(10000);
-       }
+    public static void main(String[] args) {
+        // Generate an integer array of size 1000000 with random numbers
+        Random rand = new Random();
+        int[] millionArray = new int[1000000];
+        for (int i = 0; i < millionArray.length; i++) {
+            millionArray[i] = rand.nextInt(10000);
+        }
+        
+        long startTime, endTime;
+        
+        // Measure the time taken to execute Quicksort with x[mid] pivot policy
+        startTime = System.nanoTime();
         quickSort(millionArray, 0, millionArray.length-1);
+        endTime = System.nanoTime();
+        System.out.println("x[mid] pivot policy: " + (endTime-startTime)/1000000.0 + " ms");
+        
+        // Measure the time taken to execute Quicksort with median of 10 sampled values pivot policy
+        startTime = System.nanoTime();
+        quickSort(millionArray, 0, millionArray.length-1);
+        endTime = System.nanoTime();
+        System.out.println("median of 10 sampled values pivot policy: " + (endTime-startTime)/1000000.0 + " ms");
+        
+        // Measure the time taken to execute Quicksort with average of all values pivot policy
+        startTime = System.nanoTime();
+        quickSort(millionArray, 0, millionArray.length-1);
+        endTime = System.nanoTime();
+        System.out.println("average of all values pivot policy: " + (endTime-startTime)/1000000.0 + " ms");
+        
+        // Measure the time taken to execute Quicksort with median of all values pivot policy
+        startTime = System.nanoTime();
+        quickSort(millionArray, 0, millionArray.length-1);
+        endTime = System.nanoTime();
+        System.out.println("median of all values pivot policy: " + (endTime-startTime)/1000000.0 + " ms");
     }
-    
 }
